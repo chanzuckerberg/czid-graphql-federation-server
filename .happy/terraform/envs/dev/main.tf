8216e4882bd0de081eb35eb17a42588e29246e62
Jessica Gadling <jgadling@chanzuckerberg.com>
2023-03-10 16:08:57 -0800

    Start getting ready for happy.

---
+++ .happy/terraform/envs/dev/main.tf
@@ -0,0 +1,24 @@
+module "stack" {
+  source              = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-eks?ref=main"
+  happy_config_secret = var.happy_config_secret
+  image_tag           = var.image_tag
+  happymeta_          = var.happymeta_
+  image_tags          = jsondecode(var.image_tags)
+  stack_name          = var.stack_name
+  deployment_stage    = "dev"
+  stack_prefix        = "/${var.stack_name}"
+  k8s_namespace       = "czid-dev-happy-happy-env"
+  services = {
+    czid = {
+      name                = "gql-federation",
+      desired_count       = 2,
+      port                = 4444,
+      memory              = "100Mi"
+      cpu                 = "100m"
+      health_check_path   = "/",
+      service_type        = "PRIVATE"
+    }
+  }
+  tasks = {
+  }
+}

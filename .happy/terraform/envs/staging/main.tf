a6cd41bf050b4e72395b9e7d96be2a4c4ae531f9
Jessica Gadling <jgadling@chanzuckerberg.com>
2023-03-31 17:30:59 -0700

    test targetgroupbinding

--- .happy/terraform/envs/staging/main.tf
+++ .happy/terraform/envs/staging/main.tf
@@ -1,3 +1,8 @@
+locals {
+  k8s_namespace    = "czid-staging-happy-happy-env"
+  target_group_arn = "arn:aws:elasticloadbalancing:us-west-2:732052188396:targetgroup/testing-deleteme/303f245750c9948a"
+}
+
 module "stack" {
   source           = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-eks?ref=main"
   image_tag        = var.image_tag
@@ -5,7 +10,7 @@ module "stack" {
   stack_name       = var.stack_name
   deployment_stage = "staging"
   stack_prefix     = "/${var.stack_name}"
-  k8s_namespace    = "czid-staging-happy-happy-env"
+  k8s_namespace    = local.k8s_namespace
   additional_env_vars = {
     API_URL = "https://sandbox.czid.org"
   }
@@ -17,13 +22,34 @@ module "stack" {
       memory            = "1500Mi",
       cpu               = "1500m",
       health_check_path = "/health",
-      service_type      = "INTERNAL",
       // INTERNAL - OIDC protected ALB
       // EXTERNAL - external ALB
       // PRIVATE - cluster IP only, no ALB at all
+      service_type      = "INTERNAL",
       platform_architecture = "amd64",
     }
   }
   tasks = {
+  }
+}
+
+resource "kubernetes_manifest" "custom_target_group" {
+  manifest = {
+    apiVersion = "elbv2.k8s.aws/v1beta1"
+    kind = "TargetGroupBinding"
+    metadata = {
+        name = "testtargetgroupstaging1"
+        namespace = local.k8s_namespace
+        labels = {
+          "ingress.k8s.aws/stack" = "service-staging-stack-gql-testme"
+        }
+    }
+    spec = {
+        serviceRef = {
+            name = "${var.stack_name}-gql"
+            port = 4444
+        }
+        targetGroupARN = local.target_group_arn
+    }
   }
 }

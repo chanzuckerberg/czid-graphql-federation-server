bc7524d42eef2adb7a860d960932d1919271921d
Saquib Kothawala <saquib.kothawala@contractor.chanzuckerberg.com>
2023-08-18 13:49:28 -0400

    latest chages

--- resolvers.ts
+++ resolvers.ts
@@ -1,12 +1,33 @@
 // resolvers.ts
 import fetch from 'node-fetch';
+import { Resolvers } from './.mesh';
 
-export default {
-  Query: {
-    CZID: async () => {
-      const response = await fetch('http://web:3001/graphql');
-      const data = await response.json();
-      return data;
-    },
+// export default {
+//   Query: {
+//     CZID: async () => {
+//       const response = await fetch('http://web:3001/graphql');
+//       const data = await response.json();
+//       return data;
+//     },
+//     Background: async () => {
+//       console.log('Background resolver');
+//     },
+//   },
+// };
+
+export const resolvers: Resolvers = {
+  // Background: {
+  //   resolve(root, args, context, info) {
+  //     console.log('Background resolver');
+  //   }
+  // },
+  Background: {
+    other_backgrounds: async (root, args, context, info) => {
+      console.log('other_backgrounds resolver');
+      return [];
+    }
   },
-};
+
+}
+
+// export default resolvers;

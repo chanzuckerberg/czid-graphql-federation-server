d8e6c131bffaa5eba40bc2d435ee747e3070775b
Saquib Kothawala <saquib.kothawala@contractor.chanzuckerberg.com>
2023-08-28 16:42:20 -0400

    addressed PR comments

--- transforms.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    snapshotShareBackgroundResolver: next => (root, args, context, info) => {
-        console.log('getting here snapshotShareBackgroundResolver')
-        return next(root, args, context, info)
-    }
-  }

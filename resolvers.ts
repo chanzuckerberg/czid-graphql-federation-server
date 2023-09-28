32688f6473bb65aa8611acbf180c4d71c878f0a4
Saquib Kothawala <saquib.kothawala@contractor.chanzuckerberg.com>
2023-09-28 14:27:35 -0400

    set the resolver and meshrc for coverageVizSummary

--- resolvers.ts
+++ resolvers.ts
@@ -5,14 +5,20 @@ import { get } from "./utils";
 export const resolvers: Resolvers = {
   Query: {
     AmrWorkflowResults: async (root, args, context, info) => {
-      const { quality_metrics, report_table_data } = await get(`http://web:3001/workflow_runs/${args.workflowRunId}/results`, context);
+      const { quality_metrics, report_table_data } = await get(
+        `http://web:3001/workflow_runs/${args.workflowRunId}/results`,
+        context
+      );
       return {
         metric_amr: quality_metrics,
         amr_hit: report_table_data,
       };
     },
     Background: async (root, args, context, info) => {
-      const { other_backgrounds, owned_backgrounds } = await get(`http://web:3001/pub/${args.snapshotShareId}/backgrounds.json`, context);
+      const { other_backgrounds, owned_backgrounds } = await get(
+        `http://web:3001/pub/${args.snapshotShareId}/backgrounds.json`,
+        context
+      );
       const ret = other_backgrounds.concat(owned_backgrounds);
       return ret.map((item: any) => {
         return {
@@ -24,7 +30,7 @@ export const resolvers: Resolvers = {
     Samples: async (root, args, context, info) => {
       const { samples } = await get(
         `http://web:3001/samples/index_v2.json?projectId=${args.projectId}&snapshotShareId=&basic=true`,
-        context,
+        context
       );
       return samples.map((item: any) => {
         return {
@@ -41,7 +47,10 @@ export const resolvers: Resolvers = {
       }, []);
     },
     ConsensusGenomeWorkflowResults: async (root, args, context, info) => {
-      const { coverage_viz, quality_metrics, taxon_info } = await get(`http://web:3001/workflow_runs/${args.workflowRunId}/results`, context);
+      const { coverage_viz, quality_metrics, taxon_info } = await get(
+        `http://web:3001/workflow_runs/${args.workflowRunId}/results`,
+        context
+      );
       return {
         metric_consensus_genome: {
           ...quality_metrics,
@@ -54,8 +63,35 @@ export const resolvers: Resolvers = {
             id: taxon_info.taxon_id,
             name: taxon_info.taxon_name,
           },
-        }
+        },
       };
-    }, 
+    },
+    CoverageVizSummary: async (root, args, context, info) => {
+      // should be fetched using pipeline run id instead of sample id
+      // from the new backend
+      console.log("getting here");
+      try {
+        const coverage_viz_summary = await get(
+          `https://web:3001/samples/${args.sampleId}/coverage_viz_summary`,
+          context
+        );
+        console.log(coverage_viz_summary);
+        const return_obj: any[] = [];
+        for (const key in coverage_viz_summary) {
+          for (const accension of coverage_viz_summary[key][
+            "best_accessions"
+          ]) {
+            return_obj.push({
+              pipeline_id: key,
+              ...accension,
+            });
+          }
+        }
+        return return_obj;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+    },
   },
 };

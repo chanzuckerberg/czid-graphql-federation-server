ca1e2d30c8503482f03c41e3112c2562649ac90b
Suzette McCanny <smccanny@deloitte.ca>
2024-01-10 12:47:26 -0500

    adding two different input type options for UpdateMetadata

--- resolvers.ts
+++ resolvers.ts
@@ -398,7 +398,7 @@ export const resolvers: Resolvers = {
     UpdateMetadata: async (root, args, context, info) => {
       const body = {
         field: args?.input?.field,
-        value: args?.input?.value,
+        value: args?.input?.value.String ? args.input.value.String : args?.input?.value.query_SampleMetadata_metadata_items_location_validated_value_oneOf_1_Input,
       };
       const res = await postWithCSRF(
         `/samples/${args.sampleId}/save_metadata_v2`,

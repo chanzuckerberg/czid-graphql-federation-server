0489cb331d11a6db640bcfad1f0e0f932817dd12
Jerry Fu <2072627+jfoo1984@users.noreply.github.com>
2024-02-13 10:25:01 -0800

    Undo previous change, token is parsed in separate util function

--- utils/callGraphqlSource.ts
+++ utils/callGraphqlSource.ts
@@ -1,8 +1,7 @@
 import { ResolverContext, getEnrichedToken } from "./enrichToken";
 
 export const callGraphqlSource = async (args, context) => {
-  const enrichedTokenResponse = await getEnrichedToken(context as unknown as ResolverContext);
-  const enrichedToken = (await enrichedTokenResponse.json()).token
+  const enrichedToken = await getEnrichedToken(context as unknown as ResolverContext);
 
   // TODO: Implement the actual call to the graphql source, something like the below
   // const resp = await fetch(url, {

1cdfaae35873951c4196f69bab19fce08788ecc9
Bryan Chu <bchu@chanzuckerberg.com>
2024-02-15 14:31:36 -0800

    jsdoc

--- tests/utils/ExampleQueryFiles.ts
+++ tests/utils/ExampleQueryFiles.ts
@@ -1,6 +1,10 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 
+/**
+ * @param graphqlFile file name (excludes .graphql file extension)
+ * @returns example query as a string
+ */
 export function getExampleQuery(graphqlFile: string): string {
   return readFileSync(
     join(__dirname, `../../example-queries/${graphqlFile}.graphql`),
@@ -10,6 +14,10 @@ export function getExampleQuery(graphqlFile: string): string {
   );
 }
 
+/**
+ * @param responseFile file name (excludes .json file extension)
+ * @returns JSON.parse()d value from the response file
+ */
 export function getSampleResponse(responseFile: string): any {
   return JSON.parse(
     readFileSync(

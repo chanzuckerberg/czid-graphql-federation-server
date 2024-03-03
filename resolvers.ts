28e57e21fbb6c3e3d565cc52b06b46de266fde52
Suzette McCanny <smccanny@deloitte.ca>
2024-03-02 23:02:21 -0500

    update BulkDownload modal to receive strings if necessary

--- resolvers.ts
+++ resolvers.ts
@@ -831,7 +831,7 @@ export const resolvers: Resolvers = {
           context,
         });
         return workflowRuns.map(run => ({
-          id: run.id,
+          id: run.id.toString(),
           ownerUserId: run.owner_user_id,
           status: run.status,
         }));

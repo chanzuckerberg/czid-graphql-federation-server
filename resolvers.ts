70f83821abe615a8fe340f9d907928135a6b3652
Bryan Chu <bchu@chanzuckerberg.com>
2024-03-07 16:19:53 -0800

    init

--- resolvers.ts
+++ resolvers.ts
@@ -981,7 +981,7 @@ export const resolvers: Resolvers = {
             edges: [
               {
                 node: {
-                  entityType: "SequencingRead",
+                  entityType: "sequencing_read",
                   inputEntityId: run.sample?.info?.id?.toString(),
                 },
               },

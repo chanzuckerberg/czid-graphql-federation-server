e32dd6bcd403da08300eaca786ab1a0d435247f2
Bryan Chu <bchu@chanzuckerberg.com>
2024-03-19 11:49:32 -0700

    more tests

--- tests/WorkflowRunsAggregateQuery.test.ts
+++ tests/WorkflowRunsAggregateQuery.test.ts
@@ -1,22 +1,23 @@
-import { ExecuteMeshFn } from "@graphql-mesh/runtime";
-import { getMeshInstance } from "./utils/MeshInstance";
+import { MeshExecuteTestFunction, getMeshExecute } from "./utils/MeshInstance";
 
 import * as httpUtils from "../utils/httpUtils";
 import { getExampleQuery } from "./utils/ExampleQueryFiles";
 import { query_fedWorkflowRunsAggregate_aggregate_items } from "../.mesh";
 jest.spyOn(httpUtils, "get");
 jest.spyOn(httpUtils, "shouldReadFromNextGen");
+jest.spyOn(httpUtils, "fetchFromNextGen");
 
 beforeEach(() => {
   (httpUtils.get as jest.Mock).mockClear();
+  (httpUtils.shouldReadFromNextGen as jest.Mock).mockClear();
+  (httpUtils.fetchFromNextGen as jest.Mock).mockClear();
 });
 
 describe("workflows aggregate query:", () => {
-  let execute: ExecuteMeshFn;
+  let execute: MeshExecuteTestFunction;
 
   beforeEach(async () => {
-    const mesh$ = await getMeshInstance();
-    ({ execute } = mesh$);
+    execute = await getMeshExecute();
   });
 
   it("Returns aggregate counts for each workflow", async () => {
@@ -45,16 +46,99 @@ describe("workflows aggregate query:", () => {
       context: expect.anything(),
     });
 
-    console.log(response);
-
-    const aggregates: query_fedWorkflowRunsAggregate_aggregate_items[] = response.data.fedWorkflowRunsAggregate.aggregate;
+    const aggregates: query_fedWorkflowRunsAggregate_aggregate_items[] =
+      response.data.fedWorkflowRunsAggregate.aggregate;
 
     expect(aggregates).toHaveLength(3);
-    const cgAggregate = aggregates.find(aggregate => aggregate.groupBy?.workflowVersion?.workflow?.name === "consensus-genome");
+    const cgAggregate = aggregates.find(
+      aggregate =>
+        aggregate.groupBy?.workflowVersion?.workflow?.name ===
+        "consensus-genome",
+    );
     expect(cgAggregate?.count).toBe(1);
-    const amrAggregate = aggregates.find(aggregate => aggregate.groupBy?.workflowVersion?.workflow?.name === "amr");
+    const amrAggregate = aggregates.find(
+      aggregate => aggregate.groupBy?.workflowVersion?.workflow?.name === "amr",
+    );
     expect(amrAggregate?.count).toBe(2);
-    const mngsAggregate = aggregates.find(aggregate => aggregate.groupBy?.workflowVersion?.workflow?.name === "short-read-mngs");
+    const mngsAggregate = aggregates.find(
+      aggregate =>
+        aggregate.groupBy?.workflowVersion?.workflow?.name ===
+        "short-read-mngs",
+    );
     expect(mngsAggregate?.count).toBe(3);
+  });
+
+  it("Only returns group bys for project IDs requested for page", async () => {
+    (httpUtils.shouldReadFromNextGen as jest.Mock).mockImplementation(() =>
+      Promise.resolve(true),
+    );
+    (httpUtils.get as jest.Mock).mockImplementation(() => ({
+      projects: [
+        {
+          id: 1,
+          sample_counts: {
+            amr_runs_count: 1,
+            mngs_runs_count: 1,
+          },
+        },
+        {
+          id: 2,
+          sample_counts: {
+            amr_runs_count: 2,
+            mngs_runs_count: 2,
+          },
+        },
+        {
+          id: 3,
+          sample_counts: {
+            amr_runs_count: 3,
+            mngs_runs_count: 3,
+          },
+        },
+      ],
+    }));
+    (httpUtils.fetchFromNextGen as jest.Mock).mockImplementation(() => ({
+      aggregate: [
+        {
+          groupBy: {
+            collectionId: 2,
+            workflowVersion: {
+              workflow: {
+                name: "consensus-genome",
+              },
+            },
+            count: 2,
+          },
+        },
+        {
+          groupBy: {
+            collectionId: 3,
+            workflowVersion: {
+              workflow: {
+                name: "consensus-genome",
+              },
+            },
+            count: 3,
+          },
+        },
+      ],
+    }));
+
+    const query = getExampleQuery("workflows-aggregate-query");
+
+    const response = await execute(query, {
+      input: { where: { collectionId: { _in: [2, 3] } } },
+    });
+
+    const aggregates: query_fedWorkflowRunsAggregate_aggregate_items[] =
+      response.data.fedWorkflowRunsAggregate.aggregate;
+
+    expect(aggregates).toHaveLength(2);
+    expect(
+      aggregates.find(aggregate => aggregate.groupBy.collectionId === 2),
+    ).toBeDefined();
+    expect(
+      aggregates.find(aggregate => aggregate.groupBy.collectionId === 3),
+    ).toBeDefined();
   });
 });

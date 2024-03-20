2a49843d1ca37c01aceab43c87cf9fd4070436f8
Erin Hoops <erin.hoops+gh@chanzuckerberg.com>
2024-03-20 10:18:26 -0700

    Reformat query

--- utils/queryFormatUtils.ts
+++ utils/queryFormatUtils.ts
@@ -55,7 +55,7 @@ export const convertValidateConsensusGenomeQuery = (query: string): string => {
       // Remove fed prefix.
       .replace("fedWorkflowRuns", "workflowRuns")
       // Replace Fed arguments.
-      .replace(/input:.*\n/, "where: $where")
+      .replace(/input:.*\n/, "where: $where){\n")
       
   )
 };

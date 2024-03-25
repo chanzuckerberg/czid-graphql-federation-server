f04771495a57cfe1ebe51e9b1153d0c5255ef280
Suzette McCanny <smccanny@deloitte.ca>
2024-03-25 15:07:12 -0400

    metadata rails call added

--- resolver-functions/BulkDownloadsCGOverview.ts
+++ resolver-functions/BulkDownloadsCGOverview.ts
@@ -1,4 +1,9 @@
-import { get, postWithCSRF, shouldReadFromNextGen } from "../utils/httpUtils";
+import {
+  get,
+  getFromRails,
+  postWithCSRF,
+  shouldReadFromNextGen,
+} from "../utils/httpUtils";
 
 export const BulkDownloadsCGOverviewResolver = async (
   root,
@@ -9,6 +14,14 @@ export const BulkDownloadsCGOverviewResolver = async (
   if (!args?.input) {
     throw new Error("No input provided");
   }
+  const {
+    downloadType,
+    workflow,
+    includeMetadata,
+    workflowRunIds,
+    workflowRunIdsStrings,
+  } = args?.input;
+
   /* --------------------- Next Gen ------------------------- */
   const nextGenEnabled = await shouldReadFromNextGen(context);
   if (nextGenEnabled) {
@@ -84,19 +97,21 @@ export const BulkDownloadsCGOverviewResolver = async (
         ]),
       ],
     };
-    // TODO: Add Optional Sample Metadata
-    // if (args?.input?.includeMetadata) {
-    //   call rails sample metadata query
-    //}
+    // TODO: Suzette & Jerry - Add Optional Sample Metadata
+    if (args?.input?.includeMetadata) {
+      const body = {
+        sample_ids: workflowRunIdsStrings,
+      };
+      const sampleMetadataRes = await postWithCSRF({
+        url: `/bulk_downloads/consensus_genome_sample_metadata`,
+        body,
+        args,
+        context,
+      });
+      console.log("sampleMetadataRes", sampleMetadataRes);
+    }
     return formattedForCSV;
   }
-  const {
-    downloadType,
-    workflow,
-    includeMetadata,
-    workflowRunIds,
-    workflowRunIdsStrings,
-  } = args?.input;
 
   //array of strings to array of numbers
   const workflowRunIdsNumbers = workflowRunIdsStrings?.map(

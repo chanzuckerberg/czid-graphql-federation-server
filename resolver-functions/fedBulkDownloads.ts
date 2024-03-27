c2155b581ace31cc1f67d3ba86e73f968c0fe66b
Suzette McCanny <smccanny@deloitte.ca>
2024-03-27 10:47:34 -0400

    only call for details if the download is successfull

--- resolver-functions/fedBulkDownloads.ts
+++ resolver-functions/fedBulkDownloads.ts
@@ -27,32 +27,36 @@ export const fedBulkDowloadsResolver = async (root, args, context, info) => {
     context,
   });
   const mappedRes = res.map(async bulkDownload => {
-    const details = await get({
-      url: `/bulk_downloads/${bulkDownload?.id}.json`,
-      args,
-      context,
-    });
-    const url = details?.bulk_download?.presigned_output_url;
-    const entityInputs = [
-      ...getEntityInputInfo(details?.bulk_download?.workflow_runs),
-      ...getEntityInputInfo(details?.bulk_download?.pipeline_runs),
-    ];
-    const params: {
+    let url: string | null = null;
+    let params: {
       paramType: string;
       downloadName?: string;
       value: string;
     }[] = [];
-    Object.entries(details?.bulk_download?.params)
-      // remove "workflow" and "sample_ids" from details?.bulk_download?.params
-      .filter(param => param[0] !== "workflow" && param[0] !== "sample_ids")
-      // make params into an array of objects
-      .map((param: [string, { downloadName?: string; value: string }]) => {
-        const paramItem = {
-          paramType: snakeToCamel(param[0]),
-          ...param[1],
-        };
-        params.push(paramItem);
+    let entityInputs: any[] = [];
+    if (bulkDownload.status === "success") {
+      const details = await get({
+        url: `/bulk_downloads/${bulkDownload?.id}.json`,
+        args,
+        context,
       });
+      url = details?.bulk_download?.presigned_output_url;
+      entityInputs = [
+        ...getEntityInputInfo(details?.bulk_download?.workflow_runs),
+        ...getEntityInputInfo(details?.bulk_download?.pipeline_runs),
+      ];
+      Object.entries(details?.bulk_download?.params)
+        // remove "workflow" and "sample_ids" from details?.bulk_download?.params
+        .filter(param => param[0] !== "workflow" && param[0] !== "sample_ids")
+        // make params into an array of objects
+        .map((param: [string, { downloadName?: string; value: string }]) => {
+          const paramItem = {
+            paramType: snakeToCamel(param[0]),
+            ...param[1],
+          };
+          params.push(paramItem);
+        });
+    }
 
     const {
       id,

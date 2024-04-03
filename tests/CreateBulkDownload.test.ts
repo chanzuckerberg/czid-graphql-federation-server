a102dc34967f150492d387a56383dbef50534422
Suzette McCanny <smccanny@deloitte.ca>
2024-04-03 11:21:09 -0400

    fix tests

--- tests/CreateBulkDownload.test.ts
+++ tests/CreateBulkDownload.test.ts
@@ -5,14 +5,8 @@ import { getMeshInstance } from "./utils/MeshInstance";
 import * as httpUtils from "../utils/httpUtils";
 jest.mock("../utils/httpUtils");
 
-jest.spyOn(httpUtils, "get");
-jest.spyOn(httpUtils, "shouldReadFromNextGen");
-jest.spyOn(httpUtils, "postWithCSRF");
-
 beforeEach(() => {
   (httpUtils.postWithCSRF as jest.Mock).mockClear();
-  (httpUtils.get as jest.Mock).mockClear();
-  (httpUtils.shouldReadFromNextGen as jest.Mock).mockClear();
 });
 
 describe.only("CreateBulkDownload Query", () => {
@@ -26,19 +20,11 @@ describe.only("CreateBulkDownload Query", () => {
     query = getExampleQuery("create-bulk-download-query");
   });
 
-  beforeEach(() => {
-    (httpUtils.postWithCSRF as jest.Mock).mockClear();
-    (httpUtils.get as jest.Mock).mockClear();
-    (httpUtils.shouldReadFromNextGen as jest.Mock).mockClear();
-  });
-
-  describe("CreateBulkDownload - with nextGen OFF", () => {
+  describe("CreateBulkDownload successful response", () => {
     const createBulkDownloadResponse = getSampleResponse("fedBulkDownload");
-    (httpUtils.shouldReadFromNextGen as jest.Mock).mockReturnValueOnce(false);
-
     it("should give correct response", async () => {
-      (httpUtils.postWithCSRF as jest.Mock).mockReturnValueOnce(
-        createBulkDownloadResponse,
+      (httpUtils.postWithCSRF as jest.Mock).mockImplementation(
+        () => createBulkDownloadResponse,
       );
       const result = await execute(query, {
         authenticityToken: "authtoken1234",
@@ -51,62 +37,6 @@ describe.only("CreateBulkDownload Query", () => {
       expect(result.data.CreateBulkDownload).toStrictEqual(
         createBulkDownloadResponse,
       );
-    });
-  });
-
-  describe("CreateBulkDownload successful response - with nextGen ON", () => {
-    const createBulkDownloadResponse = getSampleResponse("fedBulkDownload");
-    (httpUtils.shouldReadFromNextGen as jest.Mock).mockReturnValueOnce(true);
-    const bulkDownloadDefaultVersion = {
-      data: {
-        workflows: [
-          {
-            defaultVersion: "0.0.3",
-          },
-        ],
-      },
-    };
-    const bulkDownloadVersionId = {
-      data: {
-        workflowVersions: [
-          {
-            id: "1234",
-          },
-        ],
-      },
-    };
-    const getFilesFromEntities = {
-      data: {
-        consensusGenomes: [
-          {
-            intermediateOutputs: {
-              id: "1234",
-            },
-          },
-        ],
-      },
-    };
-
-    const runWorkflowVersionMutation = { id: "1234" };
-    (httpUtils.get as jest.Mock).mockReturnValueOnce(
-      bulkDownloadDefaultVersion,
-    );
-    (httpUtils.get as jest.Mock).mockReturnValueOnce(bulkDownloadVersionId);
-    (httpUtils.get as jest.Mock).mockReturnValueOnce(getFilesFromEntities);
-    (httpUtils.fetchFromNextGen as jest.Mock).mockReturnValueOnce(
-      runWorkflowVersionMutation,
-    );
-
-    it("should give correct response", async () => {
-      const result = await execute(query, {
-        authenticityToken: "authtoken1234",
-        downloadType: "consensus_genome_intermediate_output_files",
-        downloadFormat: "Separate Files",
-        workflow: "consensus_genome",
-        workflowRunIds: [1991, 2007],
-        workflowRunIdsStrings: ["1991", "2007"],
-      });
-      expect(result.data.CreateBulkDownload).toStrictEqual({ id: "1234" });
     });
   });
 });
